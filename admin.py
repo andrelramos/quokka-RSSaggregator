df71da59cd3d4b9df11524c6816d1f0daa2db1cc
André Luiz de Oliveira Ramos <andrel.ramos97@gmail.com>
2016-08-27 14:04:42 -0300

    Adding aggregated topics to admin

--- admin.py
+++ admin.py
@@ -2,7 +2,7 @@
 from quokka import admin
 from quokka.utils.translation import _, _l
 from quokka.core.admin.models import ModelAdmin
-from .models import ExternalBlogs
+from .models import ExternalBlogs, AggregatedTopic
 
 
 class ExternalBlogsAdmin(ModelAdmin):
@@ -12,4 +12,14 @@ class ExternalBlogsAdmin(ModelAdmin):
     column_list = ('name', 'root_url', 'feeds_url')
     form_columns = ('name', 'root_url', 'feeds_url')
 
+
+class AggregatedTopicAdmin(ModelAdmin):
+
+    roles_accepted = ('admin', 'editor')
+    column_filters = ('title', 'date', 'blog')
+    column_searchable_list = ('title', 'topic_url')
+    column_list = ('title', 'date', 'blog', 'topic_url', 'description')
+    form_columns = ('title', 'date', 'blog', 'topic_url', 'description')
+
 admin.register(ExternalBlogs, ExternalBlogsAdmin, category=_("RSSaggregator"), name=_l("External Blogs"))
+admin.register(AggregatedTopic, AggregatedTopicAdmin, category=_("RSSaggregator"), name=_l("Aggregated Topics"))
